aeab5782bb30f3f1d2edda39eea4a2c75b7d18e4
Matias Wald <waldmatias@hotmail.com>
2023-11-12 19:54:30 -0400

    Added implementation comments

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
-import { Canister, query, text, update, Void } from 'azle';
+import { AzleBlob, Canister, query, text, update, Void } from 'azle';
 
 // This is a global variable that is stored on the heap
 let message = '';
+
+// v.1
+// type doc-item: DocItem(Blob) { item-type: text | mime-type, owner, published }
+// section -> contains -> section (recurse)
+// type user: User(Principal) { Role: Author | Editor }
+//  role: author | editor
+// author:
+//  add (doc-item) state=DRAFT, remove(doc-item)
+//  doc-item has owner author, TODO: author can transfer to another author
+// editor
+//  authorize (author) add|remove doc-item, publish (doc-item) state=PUBLISHED
+// v.2
+// type doc-item-change
+//  propose_changes(doc-item) -- merging?
+
+
 
 export default Canister({
     // Query calls complete quickly because they do not go through consensus
